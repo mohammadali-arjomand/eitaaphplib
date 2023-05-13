77941d67aeb71b2d6c2d8393333ee5e552cbed8b
MohammadAli Arjomand <ma.arjomand1387@gmail.com>
2023-05-13 15:31:58 +0330

    sendMessage method added

--- eitaaphplib.php
+++ eitaaphplib.php
@@ -1,5 +1,7 @@
 <?php
 
+define("NOW", time());
+
 // eitaa main class
 class Eitaa {
     // properties
@@ -10,6 +12,25 @@ class Eitaa {
     function __construct($token, $channel_id) {
         $this->token = $token;
         $this->channel_id = $channel_id;
+    }
+
+    function sendMessage($text, $other=[]) {
+        // send a message to channel
+        $url = "https://eitaayar.ir/api/$this->token/sendMessage";
+        $fields = array(
+            "chat_id" => $this->channel_id,
+            "text" => $text,
+        );
+        $fields += $other;
+        $request = curl_init($url);
+        curl_setopt($request, CURLOPT_POST, true);
+        curl_setopt($request, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($request, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($request, CURLOPT_POSTFIELDS, $fields);
+        curl_setopt($request, CURLOPT_RETURNTRANSFER, true);
+        $output = curl_exec($request);
+        curl_close($request);
+        return $output;
     }
 
 }
