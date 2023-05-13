b098d65f0a8a88c7a8bddb9cf325bc5cfe8077e1
MohammadAli Arjomand <ma.arjomand1387@gmail.com>
2023-05-13 15:53:11 +0330

    senFile method added

--- eitaaphplib.php
+++ eitaaphplib.php
@@ -33,4 +33,23 @@ class Eitaa {
         return $output;
     }
 
+    function sendFile($file_path, $other=[]) {
+        // send a file to channel
+        $url = "https://eitaayar.ir/api/$this->token/sendFile";
+        $fields = array(
+            'file' => new \CurlFile(realpath($file_path)),
+            'chat_id' => $this->channel_id,
+        );
+        $fields += $other;
+        $request = curl_init($url);
+        curl_setopt($request, CURLOPT_POST, true);
+        curl_setopt($request, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($request, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($request, CURLOPT_POSTFIELDS, $fields);
+        curl_setopt($request, CURLOPT_RETURNTRANSFER, true);
+        $output = curl_exec($request);
+        curl_close($request);
+        return $output;
+    }
+
 }
