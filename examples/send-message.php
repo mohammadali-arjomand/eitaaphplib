99e26fc7c54a04a129d2357ab3ea65797845f212
MohammadAli Arjomand <ma.arjomand1387@gmail.com>
2023-05-14 12:52:45 +0330

    add examples

---
+++ examples/send-message.php
@@ -0,0 +1,14 @@
+<?php
+
+// include library file in project
+include "../eitaa.php";
+
+// set token and channel id
+$token = "bot16344:6d3e4430-****-****-****-************";
+$channel_id = 23333622; // or use channel id like: "eitaa_faq" for https://eitaa.com/eitaa_faq/
+
+// create object from EitaaPHP class
+$eitaa = new EitaaPHP($token, $channel_id);
+
+// use sendMessage method for send message in your channel or supergroup
+$eitaa->sendMessage("WRITE YOUR TEXT HERE");
